0a254aedf3b9140fb2698448fd80913bf108f72f
AnastasiiaChep <nastiadp607@gmail.com>
2022-04-25 22:17:08 +0300

    урок2

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-console.log("hello")
 
-console.log('hjfnd');
+// чтоби онлайн было отображение нижно в терминале писать git push и в изменениях нажать + на файле и оставить комент и галочкку //
+ 
+var name =('Anastasiia')
+    var age =  25;
+ let name1 = ("Hello");
+console.log(name1 + ' ' + name +' ' +age);
+
+const q = [3, 4, 5];
+let k = q.map(item => {
+    console.log (item);
+    return item * 2 ;
+})
+console.log(k)
+//
+const c = [2, 3 , 4 , 5 , 10, 15];
+let d = c.filter (function(x, y, z ){    
+return 7;
+})
+ console.log(d)
+
+
+
+/
+let a = [3, 4, 5];
+ delete a[5]; 
+ //умножить каждый эллемент на 2 
+ let b = [];
+ for (let i=0; i< a.leght; i++) {
+
+     b[i] = a[i]*2;
+    }
+
+    console.log(a);
+        console.log(b);
+         
+    let n = a.map (function(x){
+    return x;
+
+    });
+console.log(n);
+ 
+
